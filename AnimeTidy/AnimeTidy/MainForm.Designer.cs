46e9e72558373fcef012f8c7a40b2ff99b3520d3
Coolxye <conan_ye@163.com>
2021-07-23 23:03:46 +0900

    DPI対応

--- AnimeTidy/AnimeTidy/MainForm.Designer.cs
+++ AnimeTidy/AnimeTidy/MainForm.Designer.cs
@@ -29,14 +29,10 @@
 		private void InitializeComponent()
 		{
 			this.toolStripMain = new System.Windows.Forms.ToolStrip();
-			this.statusStripMain = new System.Windows.Forms.StatusStrip();
-			this.tabControlMain = new System.Windows.Forms.TabControl();
-			this.tabPageAnime = new System.Windows.Forms.TabPage();
-			this.tabPageMusic = new System.Windows.Forms.TabPage();
-			this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
 			this.tsbtnNew = new System.Windows.Forms.ToolStripButton();
 			this.tsbtnOpen = new System.Windows.Forms.ToolStripButton();
 			this.tsbtnSave = new System.Windows.Forms.ToolStripButton();
+			this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
 			this.tsbtnAdd = new System.Windows.Forms.ToolStripButton();
 			this.tsbtnModify = new System.Windows.Forms.ToolStripButton();
 			this.tsbtnDuplicate = new System.Windows.Forms.ToolStripButton();
@@ -48,13 +44,17 @@
 			this.tsbtnFind = new System.Windows.Forms.ToolStripButton();
 			this.tsbtnGroup = new System.Windows.Forms.ToolStripButton();
 			this.tsbtnOverlay = new System.Windows.Forms.ToolStripButton();
+			this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+			this.tabtnBackup = new System.Windows.Forms.ToolStripButton();
+			this.statusStripMain = new System.Windows.Forms.StatusStrip();
 			this.tsslSelName = new System.Windows.Forms.ToolStripStatusLabel();
 			this.tsslSelSpace = new System.Windows.Forms.ToolStripStatusLabel();
 			this.tsslTotal = new System.Windows.Forms.ToolStripStatusLabel();
 			this.tsslTotSpace = new System.Windows.Forms.ToolStripStatusLabel();
-			this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+			this.tabControlMain = new System.Windows.Forms.TabControl();
+			this.tabPageAnime = new System.Windows.Forms.TabPage();
 			this.tabAnimes = new AnimeTidy.Tabs.TabAnime();
-			this.tabtnBackup = new System.Windows.Forms.ToolStripButton();
+			this.tabPageMusic = new System.Windows.Forms.TabPage();
 			this.toolStripMain.SuspendLayout();
 			this.statusStripMain.SuspendLayout();
 			this.tabControlMain.SuspendLayout();
@@ -63,6 +63,7 @@
 			// 
 			// toolStripMain
 			// 
+			this.toolStripMain.ImageScalingSize = new System.Drawing.Size(24, 24);
 			this.toolStripMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.tsbtnNew,
             this.tsbtnOpen,
@@ -83,64 +84,10 @@
             this.tabtnBackup});
 			this.toolStripMain.Location = new System.Drawing.Point(0, 0);
 			this.toolStripMain.Name = "toolStripMain";
-			this.toolStripMain.Size = new System.Drawing.Size(284, 25);
+			this.toolStripMain.Padding = new System.Windows.Forms.Padding(0, 0, 3, 0);
+			this.toolStripMain.Size = new System.Drawing.Size(426, 33);
 			this.toolStripMain.TabIndex = 0;
 			this.toolStripMain.Text = "toolStrip1";
-			// 
-			// statusStripMain
-			// 
-			this.statusStripMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.tsslSelName,
-            this.tsslSelSpace,
-            this.tsslTotal,
-            this.tsslTotSpace});
-			this.statusStripMain.Location = new System.Drawing.Point(0, 236);
-			this.statusStripMain.Name = "statusStripMain";
-			this.statusStripMain.Size = new System.Drawing.Size(284, 26);
-			this.statusStripMain.TabIndex = 1;
-			this.statusStripMain.Text = "statusStrip1";
-			// 
-			// tabControlMain
-			// 
-			this.tabControlMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-			this.tabControlMain.Controls.Add(this.tabPageAnime);
-			this.tabControlMain.Controls.Add(this.tabPageMusic);
-			this.tabControlMain.Location = new System.Drawing.Point(12, 28);
-			this.tabControlMain.Name = "tabControlMain";
-			this.tabControlMain.SelectedIndex = 0;
-			this.tabControlMain.ShowToolTips = true;
-			this.tabControlMain.Size = new System.Drawing.Size(260, 205);
-			this.tabControlMain.TabIndex = 2;
-			this.tabControlMain.SelectedIndexChanged += new System.EventHandler(this.tabControlMain_SelectedIndexChanged);
-			this.tabControlMain.Deselected += new System.Windows.Forms.TabControlEventHandler(this.tabControlMain_Deselected);
-			// 
-			// tabPageAnime
-			// 
-			this.tabPageAnime.Controls.Add(this.tabAnimes);
-			this.tabPageAnime.Location = new System.Drawing.Point(4, 22);
-			this.tabPageAnime.Name = "tabPageAnime";
-			this.tabPageAnime.Padding = new System.Windows.Forms.Padding(3);
-			this.tabPageAnime.Size = new System.Drawing.Size(252, 179);
-			this.tabPageAnime.TabIndex = 0;
-			this.tabPageAnime.Text = "Anime";
-			this.tabPageAnime.UseVisualStyleBackColor = true;
-			// 
-			// tabPageMusic
-			// 
-			this.tabPageMusic.Location = new System.Drawing.Point(4, 22);
-			this.tabPageMusic.Name = "tabPageMusic";
-			this.tabPageMusic.Padding = new System.Windows.Forms.Padding(3);
-			this.tabPageMusic.Size = new System.Drawing.Size(252, 179);
-			this.tabPageMusic.TabIndex = 1;
-			this.tabPageMusic.Text = "Music";
-			this.tabPageMusic.UseVisualStyleBackColor = true;
-			// 
-			// toolStripSeparator1
-			// 
-			this.toolStripSeparator1.Name = "toolStripSeparator1";
-			this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
 			// 
 			// tsbtnNew
 			// 
@@ -148,7 +95,7 @@
 			this.tsbtnNew.Image = global::AnimeTidy.Properties.Resources.TsNew;
 			this.tsbtnNew.ImageTransparentColor = System.Drawing.Color.Magenta;
 			this.tsbtnNew.Name = "tsbtnNew";
-			this.tsbtnNew.Size = new System.Drawing.Size(23, 22);
+			this.tsbtnNew.Size = new System.Drawing.Size(34, 33);
 			this.tsbtnNew.Text = "New";
 			this.tsbtnNew.Click += new System.EventHandler(this.tsbtnNew_Click);
 			// 
@@ -158,7 +105,7 @@
 			this.tsbtnOpen.Image = global::AnimeTidy.Properties.Resources.TsOpen;
 			this.tsbtnOpen.ImageTransparentColor = System.Drawing.Color.Magenta;
 			this.tsbtnOpen.Name = "tsbtnOpen";
-			this.tsbtnOpen.Size = new System.Drawing.Size(23, 22);
+			this.tsbtnOpen.Size = new System.Drawing.Size(34, 33);
 			this.tsbtnOpen.Text = "Open";
 			this.tsbtnOpen.Click += new System.EventHandler(this.tsbtnOpen_Click);
 			// 
@@ -169,9 +116,14 @@
 			this.tsbtnSave.Image = global::AnimeTidy.Properties.Resources.TsSave;
 			this.tsbtnSave.ImageTransparentColor = System.Drawing.Color.Magenta;
 			this.tsbtnSave.Name = "tsbtnSave";
-			this.tsbtnSave.Size = new System.Drawing.Size(23, 22);
+			this.tsbtnSave.Size = new System.Drawing.Size(34, 33);
 			this.tsbtnSave.Text = "Save";
 			this.tsbtnSave.Click += new System.EventHandler(this.tsbtnSave_Click);
+			// 
+			// toolStripSeparator1
+			// 
+			this.toolStripSeparator1.Name = "toolStripSeparator1";
+			this.toolStripSeparator1.Size = new System.Drawing.Size(6, 38);
 			// 
 			// tsbtnAdd
 			// 
@@ -179,7 +131,7 @@
 			this.tsbtnAdd.Image = global::AnimeTidy.Properties.Resources.TsAdd;
 			this.tsbtnAdd.ImageTransparentColor = System.Drawing.Color.Magenta;
 			this.tsbtnAdd.Name = "tsbtnAdd";
-			this.tsbtnAdd.Size = new System.Drawing.Size(23, 22);
+			this.tsbtnAdd.Size = new System.Drawing.Size(34, 33);
 			this.tsbtnAdd.Text = "Add(I)";
 			this.tsbtnAdd.Click += new System.EventHandler(this.tsbtnAdd_Click);
 			// 
@@ -190,7 +142,7 @@
 			this.tsbtnModify.Image = global::AnimeTidy.Properties.Resources.TsModify;
 			this.tsbtnModify.ImageTransparentColor = System.Drawing.Color.Magenta;
 			this.tsbtnModify.Name = "tsbtnModify";
-			this.tsbtnModify.Size = new System.Drawing.Size(23, 22);
+			this.tsbtnModify.Size = new System.Drawing.Size(34, 33);
 			this.tsbtnModify.Text = "Modify(E)";
 			this.tsbtnModify.Click += new System.EventHandler(this.tsbtnModify_Click);
 			// 
@@ -201,7 +153,7 @@
 			this.tsbtnDuplicate.Image = global::AnimeTidy.Properties.Resources.TsDuplicate;
 			this.tsbtnDuplicate.ImageTransparentColor = System.Drawing.Color.Magenta;
 			this.tsbtnDuplicate.Name = "tsbtnDuplicate";
-			this.tsbtnDuplicate.Size = new System.Drawing.Size(23, 22);
+			this.tsbtnDuplicate.Size = new System.Drawing.Size(34, 33);
 			this.tsbtnDuplicate.Text = "Duplicate(D)";
 			this.tsbtnDuplicate.Click += new System.EventHandler(this.tsbtnDuplicate_Click);
 			// 
@@ -212,7 +164,7 @@
 			this.tsbtnDelete.Image = global::AnimeTidy.Properties.Resources.TsDel;
 			this.tsbtnDelete.ImageTransparentColor = System.Drawing.Color.Magenta;
 			this.tsbtnDelete.Name = "tsbtnDelete";
-			this.tsbtnDelete.Size = new System.Drawing.Size(23, 22);
+			this.tsbtnDelete.Size = new System.Drawing.Size(34, 33);
 			this.tsbtnDelete.Text = "Delete";
 			this.tsbtnDelete.Click += new System.EventHandler(this.tsbtnDelete_Click);
 			// 
@@ -223,14 +175,14 @@
 			this.tsbtnUndo.Image = global::AnimeTidy.Properties.Resources.TsUndo;
 			this.tsbtnUndo.ImageTransparentColor = System.Drawing.Color.Magenta;
 			this.tsbtnUndo.Name = "tsbtnUndo";
-			this.tsbtnUndo.Size = new System.Drawing.Size(23, 22);
+			this.tsbtnUndo.Size = new System.Drawing.Size(34, 33);
 			this.tsbtnUndo.Text = "Undo";
 			this.tsbtnUndo.Click += new System.EventHandler(this.tsbtnUndo_Click);
 			// 
 			// toolStripSeparator2
 			// 
 			this.toolStripSeparator2.Name = "toolStripSeparator2";
-			this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+			this.toolStripSeparator2.Size = new System.Drawing.Size(6, 38);
 			// 
 			// tsbtnRefresh
 			// 
@@ -238,14 +190,14 @@
 			this.tsbtnRefresh.Image = global::AnimeTidy.Properties.Resources.TsRefresh;
 			this.tsbtnRefresh.ImageTransparentColor = System.Drawing.Color.Magenta;
 			this.tsbtnRefresh.Name = "tsbtnRefresh";
-			this.tsbtnRefresh.Size = new System.Drawing.Size(23, 22);
+			this.tsbtnRefresh.Size = new System.Drawing.Size(34, 33);
 			this.tsbtnRefresh.Text = "Refresh";
 			this.tsbtnRefresh.Click += new System.EventHandler(this.tsbtnRefresh_Click);
 			// 
 			// toolStripSeparator3
 			// 
 			this.toolStripSeparator3.Name = "toolStripSeparator3";
-			this.toolStripSeparator3.Size = new System.Drawing.Size(6, 25);
+			this.toolStripSeparator3.Size = new System.Drawing.Size(6, 38);
 			// 
 			// tsbtnFind
 			// 
@@ -253,7 +205,7 @@
 			this.tsbtnFind.Image = global::AnimeTidy.Properties.Resources.TsSearch;
 			this.tsbtnFind.ImageTransparentColor = System.Drawing.Color.Magenta;
 			this.tsbtnFind.Name = "tsbtnFind";
-			this.tsbtnFind.Size = new System.Drawing.Size(23, 22);
+			this.tsbtnFind.Size = new System.Drawing.Size(34, 33);
 			this.tsbtnFind.Text = "Find";
 			this.tsbtnFind.Click += new System.EventHandler(this.tsbtnFind_Click);
 			// 
@@ -264,7 +216,7 @@
 			this.tsbtnGroup.Image = global::AnimeTidy.Properties.Resources.TsGroup;
 			this.tsbtnGroup.ImageTransparentColor = System.Drawing.Color.Magenta;
 			this.tsbtnGroup.Name = "tsbtnGroup";
-			this.tsbtnGroup.Size = new System.Drawing.Size(23, 20);
+			this.tsbtnGroup.Size = new System.Drawing.Size(34, 52);
 			this.tsbtnGroup.Text = "Group";
 			this.tsbtnGroup.Click += new System.EventHandler(this.tsbtnGroup_Click);
 			// 
@@ -277,53 +229,14 @@
 			this.tsbtnOverlay.Image = global::AnimeTidy.Properties.Resources.TsOverlay;
 			this.tsbtnOverlay.ImageTransparentColor = System.Drawing.Color.Magenta;
 			this.tsbtnOverlay.Name = "tsbtnOverlay";
-			this.tsbtnOverlay.Size = new System.Drawing.Size(23, 20);
+			this.tsbtnOverlay.Size = new System.Drawing.Size(34, 52);
 			this.tsbtnOverlay.Text = "Overlay";
 			this.tsbtnOverlay.Click += new System.EventHandler(this.tsbtnOverlay_Click);
-			// 
-			// tsslSelName
-			// 
-			this.tsslSelName.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Right;
-			this.tsslSelName.Name = "tsslSelName";
-			this.tsslSelName.Size = new System.Drawing.Size(108, 21);
-			this.tsslSelName.Spring = true;
-			this.tsslSelName.Text = "Selected: -";
-			// 
-			// tsslSelSpace
-			// 
-			this.tsslSelSpace.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Right;
-			this.tsslSelSpace.Name = "tsslSelSpace";
-			this.tsslSelSpace.Size = new System.Drawing.Size(108, 21);
-			this.tsslSelSpace.Spring = true;
-			this.tsslSelSpace.Text = "Selected Size: -";
-			// 
-			// tsslTotal
-			// 
-			this.tsslTotal.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Right;
-			this.tsslTotal.Name = "tsslTotal";
-			this.tsslTotal.Size = new System.Drawing.Size(53, 21);
-			this.tsslTotal.Text = "Total: -";
-			// 
-			// tsslTotSpace
-			// 
-			this.tsslTotSpace.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Right;
-			this.tsslTotSpace.Name = "tsslTotSpace";
-			this.tsslTotSpace.Size = new System.Drawing.Size(80, 21);
-			this.tsslTotSpace.Text = "Total Size: -";
 			// 
 			// toolStripSeparator4
 			// 
 			this.toolStripSeparator4.Name = "toolStripSeparator4";
-			this.toolStripSeparator4.Size = new System.Drawing.Size(6, 6);
-			// 
-			// tabAnimes
-			// 
-			this.tabAnimes.AnimeInfo = null;
-			this.tabAnimes.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.tabAnimes.Location = new System.Drawing.Point(3, 3);
-			this.tabAnimes.Name = "tabAnimes";
-			this.tabAnimes.Size = new System.Drawing.Size(246, 173);
-			this.tabAnimes.TabIndex = 0;
+			this.toolStripSeparator4.Size = new System.Drawing.Size(6, 57);
 			// 
 			// tabtnBackup
 			// 
@@ -331,18 +244,114 @@
 			this.tabtnBackup.Image = global::AnimeTidy.Properties.Resources.TsBackup;
 			this.tabtnBackup.ImageTransparentColor = System.Drawing.Color.Magenta;
 			this.tabtnBackup.Name = "tabtnBackup";
-			this.tabtnBackup.Size = new System.Drawing.Size(23, 20);
+			this.tabtnBackup.Size = new System.Drawing.Size(34, 52);
 			this.tabtnBackup.Text = "Backup";
 			this.tabtnBackup.Click += new System.EventHandler(this.tabtnBackup_Click);
 			// 
+			// statusStripMain
+			// 
+			this.statusStripMain.ImageScalingSize = new System.Drawing.Size(24, 24);
+			this.statusStripMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsslSelName,
+            this.tsslSelSpace,
+            this.tsslTotal,
+            this.tsslTotSpace});
+			this.statusStripMain.Location = new System.Drawing.Point(0, 358);
+			this.statusStripMain.Name = "statusStripMain";
+			this.statusStripMain.Padding = new System.Windows.Forms.Padding(2, 0, 21, 0);
+			this.statusStripMain.Size = new System.Drawing.Size(426, 35);
+			this.statusStripMain.TabIndex = 1;
+			this.statusStripMain.Text = "statusStrip1";
+			// 
+			// tsslSelName
+			// 
+			this.tsslSelName.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Right;
+			this.tsslSelName.Name = "tsslSelName";
+			this.tsslSelName.Size = new System.Drawing.Size(201, 28);
+			this.tsslSelName.Spring = true;
+			this.tsslSelName.Text = "Selected: -";
+			// 
+			// tsslSelSpace
+			// 
+			this.tsslSelSpace.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Right;
+			this.tsslSelSpace.Name = "tsslSelSpace";
+			this.tsslSelSpace.Size = new System.Drawing.Size(201, 28);
+			this.tsslSelSpace.Spring = true;
+			this.tsslSelSpace.Text = "Selected Size: -";
+			// 
+			// tsslTotal
+			// 
+			this.tsslTotal.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Right;
+			this.tsslTotal.Name = "tsslTotal";
+			this.tsslTotal.Size = new System.Drawing.Size(74, 28);
+			this.tsslTotal.Text = "Total: -";
+			// 
+			// tsslTotSpace
+			// 
+			this.tsslTotSpace.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Right;
+			this.tsslTotSpace.Name = "tsslTotSpace";
+			this.tsslTotSpace.Size = new System.Drawing.Size(113, 28);
+			this.tsslTotSpace.Text = "Total Size: -";
+			// 
+			// tabControlMain
+			// 
+			this.tabControlMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.tabControlMain.Controls.Add(this.tabPageAnime);
+			this.tabControlMain.Controls.Add(this.tabPageMusic);
+			this.tabControlMain.Location = new System.Drawing.Point(18, 42);
+			this.tabControlMain.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.tabControlMain.Name = "tabControlMain";
+			this.tabControlMain.SelectedIndex = 0;
+			this.tabControlMain.ShowToolTips = true;
+			this.tabControlMain.Size = new System.Drawing.Size(390, 308);
+			this.tabControlMain.TabIndex = 2;
+			this.tabControlMain.SelectedIndexChanged += new System.EventHandler(this.tabControlMain_SelectedIndexChanged);
+			this.tabControlMain.Deselected += new System.Windows.Forms.TabControlEventHandler(this.tabControlMain_Deselected);
+			// 
+			// tabPageAnime
+			// 
+			this.tabPageAnime.Controls.Add(this.tabAnimes);
+			this.tabPageAnime.Location = new System.Drawing.Point(4, 28);
+			this.tabPageAnime.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.tabPageAnime.Name = "tabPageAnime";
+			this.tabPageAnime.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.tabPageAnime.Size = new System.Drawing.Size(382, 276);
+			this.tabPageAnime.TabIndex = 0;
+			this.tabPageAnime.Text = "Anime";
+			this.tabPageAnime.UseVisualStyleBackColor = true;
+			// 
+			// tabAnimes
+			// 
+			this.tabAnimes.AnimeInfo = null;
+			this.tabAnimes.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tabAnimes.Location = new System.Drawing.Point(4, 4);
+			this.tabAnimes.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+			this.tabAnimes.Name = "tabAnimes";
+			this.tabAnimes.Size = new System.Drawing.Size(374, 268);
+			this.tabAnimes.TabIndex = 0;
+			// 
+			// tabPageMusic
+			// 
+			this.tabPageMusic.Location = new System.Drawing.Point(4, 28);
+			this.tabPageMusic.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.tabPageMusic.Name = "tabPageMusic";
+			this.tabPageMusic.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.tabPageMusic.Size = new System.Drawing.Size(382, 276);
+			this.tabPageMusic.TabIndex = 1;
+			this.tabPageMusic.Text = "Music";
+			this.tabPageMusic.UseVisualStyleBackColor = true;
+			// 
 			// MainForm
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(284, 262);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+			this.ClientSize = new System.Drawing.Size(426, 393);
 			this.Controls.Add(this.tabControlMain);
 			this.Controls.Add(this.statusStripMain);
 			this.Controls.Add(this.toolStripMain);
+			this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.Name = "MainForm";
 			this.Text = "AnimeTidy";
 			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
